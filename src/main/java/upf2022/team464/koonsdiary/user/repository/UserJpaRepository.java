26a03fc42441d86697a8070e29ea0dceea9a3405
ingkoon <ing97220@naver.com>
2022-12-01 23:31:16 +0900

    feat: 아이디 체크 추가

--- src/main/java/upf2022/team464/koonsdiary/user/repository/UserJpaRepository.java
+++ src/main/java/upf2022/team464/koonsdiary/user/repository/UserJpaRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import upf2022.team464.koonsdiary.user.domain.User;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Long>, UserRepository{
+
+    Optional<User> findByAccount(String Account);
+    Optional<List<User>> findAllByAccount(String account);
 }
