e9a5682881ab09ff19e086be8e039b92367dbf2c
ingkoon <ing97220@naver.com>
2022-12-08 01:03:35 +0900

    feature: applicaiton 추가

---
+++ koonsdiary-api/src/main/java/upf2022/team464/KoonsDiaryApplication.java
@@ -0,0 +1,11 @@
+package upf2022.team464;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoonsDiaryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KoonsDiaryApplication.class, args);
+    }
+}
--- koonsdiary-api/src/main/java/upf2022/team464/Main.java
+++
@@ -1,7 +0,0 @@
-package upf2022.team464;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
